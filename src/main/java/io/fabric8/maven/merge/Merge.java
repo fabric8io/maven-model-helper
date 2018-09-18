e37296929c3311070a8f6987237bd18b96fdae47
George Gastaldi <gegastaldi@gmail.com>
2018-09-18 11:42:38 -0300

    feat: support multiple sources

--- src/main/java/io/fabric8/maven/merge/Merge.java
+++ src/main/java/io/fabric8/maven/merge/Merge.java
@@ -20,14 +20,20 @@ public class Merge {
      * After execution, the target pom.xml should contain the merged contents from both poms
      */
     public static void main(String[] args) {
+        if (args.length < 2) {
+            System.err.println("You need to specify the target and at least one source file");
+            System.exit(1);
+        }
         Path targetPath = Paths.get(args[0]).toAbsolutePath();
-        Path sourcePath = Paths.get(args[1]).toAbsolutePath();
         Model targetModel = Maven.readModel(targetPath);
-        Model sourceModel = Maven.readModel(sourcePath);
-
+        boolean sourceDominant = Boolean.getBoolean("sourceDominant");
         ModelMerger merger = new SmartModelMerger();
-        merger.merge(targetModel, sourceModel, false, null);
 
+        for (int i = 1; i < args.length; i++) {
+            Path sourcePath = Paths.get(args[i]).toAbsolutePath();
+            Model sourceModel = Maven.readModel(sourcePath);
+            merger.merge(targetModel, sourceModel, sourceDominant, null);
+        }
         Maven.writeModel(targetModel);
     }
 }
