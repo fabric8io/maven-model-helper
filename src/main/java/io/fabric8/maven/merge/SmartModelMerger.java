f7fd2c440f1d88a8a00a753bceaad3cfc4ea52a8
George Gastaldi <gegastaldi@gmail.com>
2018-11-27 00:05:53 -0200

    fix: profiles should now be correctly merged

--- src/main/java/io/fabric8/maven/merge/SmartModelMerger.java
+++ src/main/java/io/fabric8/maven/merge/SmartModelMerger.java
@@ -1,8 +1,17 @@
 package io.fabric8.maven.merge;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
 import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.ModelBase;
+import org.apache.maven.model.Profile;
 import org.apache.maven.model.merge.ModelMerger;
 
 /**
@@ -13,6 +22,50 @@ public class SmartModelMerger extends ModelMerger {
     @Override
     protected Object getDependencyKey(Dependency dependency) {
         return new DependencyKey(dependency);
+    }
+
+    @Override
+    protected Object getProfileKey(Profile profile) {
+        return new ProfileKey(profile);
+    }
+
+    @Override
+    protected void mergeModelBase_Modules(ModelBase target, ModelBase source, boolean sourceDominant, Map<Object, Object> context) {
+        Set<String> set = new LinkedHashSet<>();
+        set.addAll(source.getModules());
+        set.addAll(target.getModules());
+        target.setModules(new ArrayList<>(set));
+    }
+
+    @Override
+    protected void mergeModel_Profiles(Model target, Model source, boolean sourceDominant,
+                                       Map<Object, Object> context) {
+        List<Profile> src = source.getProfiles();
+        if (!src.isEmpty()) {
+            List<Profile> tgt = target.getProfiles();
+            Map<Object, Profile> merged = new LinkedHashMap<>((src.size() + tgt.size()) * 2);
+
+            for (Profile element : tgt) {
+                Object key = getProfileKey(element);
+                merged.put(key, element);
+            }
+
+            for (Profile element : src) {
+                Object key = getProfileKey(element);
+                if (sourceDominant || !merged.containsKey(key)) {
+                    Profile targetProfile = merged.get(key);
+                    if (targetProfile != null) {
+                        // Target Profile already exists. Merge contents
+                        mergeProfile(targetProfile, element, sourceDominant, context);
+                        merged.put(key, targetProfile);
+                    } else {
+                        merged.put(key, element);
+                    }
+                }
+            }
+
+            target.setProfiles(new ArrayList<>(merged.values()));
+        }
     }
 
     // Maven's Dependency class does not implement equals/hashCode
@@ -36,6 +89,28 @@ public class SmartModelMerger extends ModelMerger {
         @Override
         public int hashCode() {
             return Objects.hash(dependency.getGroupId(), dependency.getArtifactId());
+        }
+    }
+
+    // Maven's Profile class does not implement equals/hashCode
+    private class ProfileKey {
+        private final Profile profile;
+
+        private ProfileKey(Profile profile) {
+            this.profile = profile;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            ProfileKey that = (ProfileKey) o;
+            return Objects.equals(profile.getId(), that.profile.getId());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(profile.getId());
         }
     }
 }
--- src/test/java/io/fabric8/maven/merge/SmartModelMergerTest.java
+++ src/test/java/io/fabric8/maven/merge/SmartModelMergerTest.java
@@ -1,7 +1,10 @@
 package io.fabric8.maven.merge;
 
+import java.util.Arrays;
+
 import org.apache.maven.model.Dependency;
 import org.apache.maven.model.Model;
+import org.apache.maven.model.Profile;
 import org.apache.maven.model.merge.ModelMerger;
 import org.junit.Test;
 
@@ -35,4 +38,56 @@ public class SmartModelMergerTest {
 
         assertThat(target.getDependencies()).contains(dependency);
     }
+
+    @Test
+    public void should_copy_profiles() {
+        ModelMerger merger = new SmartModelMerger();
+        Model source = new Model();
+        source.setGroupId("org.example");
+        source.setArtifactId("example");
+        source.setVersion("1.0");
+
+        Profile profile = new Profile();
+        profile.setId("foo");
+        profile.setModules(Arrays.asList("A", "B", "C"));
+        source.addProfile(profile);
+
+        Model target = new Model();
+        target.setGroupId("org.example");
+        target.setArtifactId("example");
+        target.setVersion("2.0");
+
+        merger.merge(target, source, false, null);
+
+        assertThat(target.getProfiles().contains(profile));
+    }
+
+    @Test
+    public void should_merge_profiles() {
+        ModelMerger merger = new SmartModelMerger();
+        Model source = new Model();
+        source.setGroupId("org.example");
+        source.setArtifactId("example");
+        source.setVersion("1.0");
+
+        Profile profile = new Profile();
+        profile.setId("foo");
+        profile.setModules(Arrays.asList("A", "B", "C"));
+        source.addProfile(profile);
+
+        Model target = new Model();
+        target.setGroupId("org.example");
+        target.setArtifactId("example2");
+        target.setVersion("2.0");
+
+        Profile profile2 = new Profile();
+        profile2.setId("foo");
+        profile2.setModules(Arrays.asList("D", "E", "F"));
+        target.addProfile(profile2);
+        merger.merge(target, source, true, null);
+
+        assertThat(target.getProfiles()).hasSize(1);
+        assertThat(target.getProfiles().get(0).getModules()).containsExactly("A", "B", "C", "D", "E", "F");
+    }
+
 }
