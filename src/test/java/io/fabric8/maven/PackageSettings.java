b44b361f9a826c5fde47376af558dd268727bccc
George Gastaldi <gegastaldi@gmail.com>
2024-03-18 16:57:36 -0300

    Move approvaltests to src/test/resources

---
+++ src/test/java/io/fabric8/maven/PackageSettings.java
@@ -0,0 +1,13 @@
+package io.fabric8.maven;
+
+/**
+ * This class is used by the ApprovalTests framework
+ *
+ * @see <a href=
+ *      "https://github.com/approvals/ApprovalTests.Java/blob/master/approvaltests/docs/Configuration.md#package-level-settings">ApprovalTests
+ *      Configuration</a>
+ */
+public class PackageSettings {
+    public static String ApprovalBaseDirectory = "../resources";
+
+}
